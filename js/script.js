5aea1db118e1cd933bbe6ecc0cf0bdf39a9df3d9
Fayzulla <fayzullarakhmatullayev@gmail.com>
2021-11-30 11:27:43 +0500

    update

--- js/script.js
+++ js/script.js
@@ -8,4 +8,14 @@ $(document).ready(function () {
     nextArrow: '<img class="next-arrow" src="./img/icons/next-arrow.svg" alt="next-arrow">',
     prevArrow: '<img class="prev-arrow" src="./img/icons/prev-arrow.svg" alt="prev-arrow">',
   })
+  $('.service-slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    centerMode: true,
+    centerPadding: 0,
+    arrows: false,
+    dots: true,
+  })
 });
